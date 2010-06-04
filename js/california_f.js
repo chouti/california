173fe575adc9a57b3d84e24ab56be5514d8b06c9
chouti <chouti@gmail.com>
2010-06-04 06:43:03 +0000

    change to JSON

--- js/california_f.js
+++ js/california_f.js
@@ -12,55 +12,29 @@ $('#loadf').fadeIn('fast',function(){
 
 	var year=d.getFullYear()-1;
 
-	//var page = (Math.floor(Math.random()*21))+1;
-	var apikey = '34ff39837fe17beaf0502aded9e78a4f';
+	
+	var apikey="34ff39837fe17beaf0502aded9e78a4f";
+	
+	
 	
 	$.getJSON('http://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key='+apikey+'&per_page=500&date='+year+'-'+month+'-'+date+'&format=json&jsoncallback=?',
 		function(data){
-			$.each($(data).find('photo'), function(i,item){
+			var imagearry=data.photos.photo.sort(function(){
+				return Math.random()>0.5?-1:1;
+			});
+			var imagelist=imagearry.slice(1,21);
+			$.each(imagelist, function(i,item){
 				var url = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_s.jpg';
-				var dest = 'http://www.flickr.com/photos/'+item.ower+'/'+item.id+'/';
+				var dest = 'http://www.flickr.com/photos/'+item.owner+'/'+item.id+'/';
 				var image = '<a href="'+dest+'" target="_blank"><img src="'+url+'" /></a>';
+				
+				$('#flickr').append(image);
+				$('#loadf').fadeOut('fast',function(){
+					$('#flickr').fadeIn('fast');
 			});
-			$('#flickr').append(data.image);
-			$('#loadf').fadeOut('fast',function(){
-				$('#flickr').fadeIn('fast');
 			});
+			
+
 		});
 
-/*
-	$.ajax({
-	   type: "GET",
-	   url: "http://api.flickr.com/services/rest/?",
-	   dataType: "xml",	
-	   data: "method=flickr.interestingness.getList&" +
-			"api_key=34ff39837fe17beaf0502aded9e78a4f&" +
-			"per_page=500&"+
-			"date="+year+
-			"-"+month+
-			"-"+date,
-
-   success:function(msg){$(msg).find('photo').each(function(){
-	var id     = $(this).attr('id');
-	var owner  = $(this).attr('owner');
-	var farm   = $(this).attr('farm');
-	var server = $(this).attr('server');
-	var secret = $(this).attr('secret');
-	var url    = "http://farm" +farm+ ".static.flickr.com/" +server+ "/" +id+ "_" +secret+ "_s.jpg";
-	var dest   = "http://www.flickr.com/photos/"+owner+"/"+id+"/";
-	var image  = '<a href="'+dest+'" target="_blank"><img src="'+url+'" /></a>';	
-	$('#flickr').append(image);
-	$('#loadf').fadeOut('fast',function(){
-		$('#flickr').fadeIn('fast');
-	});
-   });
-   },
-   error:function(){
-	$('#flickr').html('<p>Error in connection, Please check your internet connection</p>');
-	$('#loadf').fadeOut('fast',function(){
-		$('#flickr').fadeIn('fast');
-	});
-   }
- });
- */
 });
--- js/california_y.js
+++ js/california_y.js
@@ -1,6 +1,6 @@
 $('#loady').fadeIn('fast',function(){
 	$('#yupoo').empty();
-
+/*
 	var d = new Date();
 	var date= d.getDate();
 	if (date < 10)
@@ -11,16 +11,39 @@ $('#loady').fadeIn('fast',function(){
 		month = "0"+month;
 
 	var year=d.getFullYear()-1;
+*/
+	var apikey="bdda85402c863453487c2a7a61a709fd";
 
-	var page = (Math.floor(Math.random()*10))+1;
+	$.getJSON('http://www.yupoo.com/api/rest/?method=yupoo.interestingness.getList&api_key='+apikey+'&per_page=500&format=json&jsoncallback=?',
+		function(data){
+			var imagearry=data.result.photos.sort(function(){
+				return Math.random()>0.5?-1:1;
+			});
+			var imagelist=imagearry.slice(1,21);
+			
+			$.each(imagelist, function(i,item){
+				var idarrary = item.id.split("-");
+				var photoid = idarrary[1];
+				var url = 'http://photo' +item.host+ '.yupoo.com/' +item.dir+ '/' +item.filename+ '/square/';
+				var dest = 'http://www.yupoo.com/photos/'+item.ownername+'/'+item.photoid+'/';
+				var image = '<a href="'+dest+'" target="_blank"><img src="'+url+'" /></a>';
+				console.log(url);
+				$('#yupoo').append(image);
+				$('#loady').fadeOut('fast',function(){
+					$('#yupoo').fadeIn('fast');
+			});
+			});
 
+
+		});
+/*
 	$.ajax({
 	   type: "GET",
 	   url: "http://www.yupoo.com/api/rest/?",
 	   dataType: "xml",	
 	   data: "method=yupoo.interestingness.getList&" +
 			"api_key=bdda85402c863453487c2a7a61a709fd&" +
-			"per_page=20",
+			"per_page=500",
 
    success:function(msg){$(msg).find('photo').each(function(){
 	var id     = $(this).attr('id');
@@ -51,4 +74,5 @@ $('#loady').fadeIn('fast',function(){
 	});
    }
  });
+ */
 });

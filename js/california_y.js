4019551ac177444768c2e56068f1a37d3a6f4333
chouti <chouti@gmail.com>
2010-06-04 06:59:18 +0000

    fix the json callback issue.

--- js/california_y.js
+++ js/california_y.js
@@ -14,7 +14,7 @@ $('#loady').fadeIn('fast',function(){
 */
 	var apikey="bdda85402c863453487c2a7a61a709fd";
 
-	$.getJSON('http://www.yupoo.com/api/rest/?method=yupoo.interestingness.getList&api_key='+apikey+'&per_page=500&format=json&jsoncallback=?',
+	$.getJSON('http://www.yupoo.com/api/rest/?method=yupoo.interestingness.getList&api_key='+apikey+'&per_page=500&format=json&callback=?',
 		function(data){
 			var imagearry=data.result.photos.sort(function(){
 				return Math.random()>0.5?-1:1;
@@ -25,9 +25,9 @@ $('#loady').fadeIn('fast',function(){
 				var idarrary = item.id.split("-");
 				var photoid = idarrary[1];
 				var url = 'http://photo' +item.host+ '.yupoo.com/' +item.dir+ '/' +item.filename+ '/square/';
-				var dest = 'http://www.yupoo.com/photos/'+item.ownername+'/'+item.photoid+'/';
+				var dest = 'http://www.yupoo.com/photos/'+item.ownername+'/'+photoid+'/';
 				var image = '<a href="'+dest+'" target="_blank"><img src="'+url+'" /></a>';
-				console.log(url);
+				console.log(dest);
 				$('#yupoo').append(image);
 				$('#loady').fadeOut('fast',function(){
 					$('#yupoo').fadeIn('fast');
@@ -36,43 +36,4 @@ $('#loady').fadeIn('fast',function(){
 
 
 		});
-/*
-	$.ajax({
-	   type: "GET",
-	   url: "http://www.yupoo.com/api/rest/?",
-	   dataType: "xml",	
-	   data: "method=yupoo.interestingness.getList&" +
-			"api_key=bdda85402c863453487c2a7a61a709fd&" +
-			"per_page=500",
-
-   success:function(msg){$(msg).find('photo').each(function(){
-	var id     = $(this).attr('id');
-	var owner  = $(this).attr('owner');
-	var idarrary = id.split("-");
-	var photoid = idarrary[1];
-	
-	var ownername   = $(this).attr('ownername');
-	var title = $(this).attr('title');
-	var bucket = $(this).attr('bucket');
-	var key = $(this).attr('key');
-	var host = $(this).attr('host');
-	var dir = $(this).attr('dir');
-	var filename = $(this).attr('filename');
-	var url    = "http://photo" +host+ ".yupoo.com/" +dir+ "/" +filename+ "/square/";
-	var dest   = "http://www.yupoo.com/photos/"+ownername+"/"+photoid+"/";
-	var image  = '<a href="'+dest+'" target="_blank"><img src="'+url+'" /></a>';	
-	$('#yupoo').append(image);
-	$('#loady').fadeOut('fast',function(){
-		$('#yupoo').fadeIn('fast');
-	});
-   });
-   },
-   error:function(){
-	$('#yupoo').html('<p>Error in connection, Please check your internet connection</p>');
-	$('#loady').fadeOut('fast',function(){
-		$('#yupoo').fadeIn('fast');
-	});
-   }
- });
- */
 });
